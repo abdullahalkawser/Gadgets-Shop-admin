d13ea6fd7a3d163db7422ec0af96f3cb18318e8b
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-03-18 23:26:10 +0600

    some change code api

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const MyComponent = () => {
   // Function to fetch data from the API
   const fetchData = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/products");
+      const response = await fetch("/api/products");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
